4ed4c492f0c1bee9c7b9f409d9fb720b5b4c6ebd
ManoLo2ManoLo <manuel_canas@aol.com>
2021-10-05 06:22:47 -0400

    made highscore page, and added audio
    the audio is to determine wheather or not you are correct or inccorect

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 const errorhomeBtn = document.getElementById('errorhomeBtn');
+const submitBtn = document.getElementById('submitBtn');
 
 const questionEl = document.getElementById('question');
 const answer1 = document.getElementById('answer1');
@@ -9,7 +10,9 @@ const answer4 = document.getElementById('answer4');
 const scoreEl = document.getElementById('scoreEl');
 const finalscoreEl = document.getElementById('finalscoreEl');
 const giphyEl = document.getElementById('giphyEl');
-const randomFactEl = document.getElementById('randomFactEl')
+const randomFactEl = document.getElementById('randomFactEl');
+const nameEl = document.getElementById('nameEl');
+const highScoreListEl = document.getElementById('highScoreListEl');
 
 
 var catNumber = 0;
@@ -44,10 +47,11 @@ function generateQuiz() {
 function randomQuiz() {
     document.getElementById("catPage").style.display = "none";
     document.getElementById("diffPage").style.display = "none";
+    document.getElementById("highScorePage").style.display = "none";
 
     var apiUrl = 'https://opentdb.com/api.php?amount=10&type=multiple';
     catName = 'Random';
-    
+
     fetch(apiUrl).then(function(response) {
         if (response.ok) {
             response.json().then(function(data) {
@@ -104,8 +108,10 @@ function checkAnswer(answer) {
     if (answer === correctAnswer) {
         score=score+10;
         questionCounter++;
+        playCorrectAudio();
         setCounterText();
     } else {
+        playIncorrectAudio();
         questionCounter++;
     }
 
@@ -115,81 +121,41 @@ function checkAnswer(answer) {
     }
 }
 
+function playCorrectAudio() {
+    const correctAudio = new Audio('assets/audio/correct.mp3');
+    correctAudio.play();
+}
+
+function playIncorrectAudio() {
+    const incorrectAudio = new Audio('assets/audio/incorrect.mp3');
+    incorrectAudio.play();
+}
 // Category Selectory
 function catSelector(category) {
-    if (category === '1') {
-        catNumber = 9;
-        catName = 'General';
-    } else if (category === '2') {
-        catNumber = 10;
-        catName = 'Books';
-    } else if (category === '3') {
-        catNumber = 11;
-        catName = 'Film';
-    } else if (category === '4') {
-        catNumber = 12;
-        catName = 'Music';
-    } else if (category === '5') {
-        catNumber = 13;
-        catName = 'Theatre';
-    } else if (category === '6') {
-        catNumber = 14;
-        catName = 'Television';
-    } else if (category === '7') {
-        catNumber = 15;
-        catName = 'Video Games';
-    } else if (category === '8') {
-        catNumber = 16;
-        catName = 'Board Games';
-    } else if (category === '9') {
-        catNumber = 17;
-        catName = 'Science';
-    } else if (category === '10') {
-        catNumber = 18;
-        catName = 'Computers';
-    } else if (category === '11') {
-        catNumber = 19;
-        catName = 'Math';
-    } else if (category === '12') {
-        catNumber = 20;
-        catName = 'Mythology';
-    } else if (category === '13') {
-        catNumber = 21;
-        catName = 'Sports';
-    } else if (category === '14') {
-        catNumber = 22;
-        catName = 'Geography';
-    } else if (category === '15') {
-        catNumber = 23;
-        catName = 'History';
-    } else if (category === '16') {
-        catNumber = 24;
-        catName = 'Politics';
-    } else if (category === '17') {
-        catNumber = 25;
-        catName = 'Art';
-    } else if (category === '18') {
-        catNumber = 26;
-        catName = 'Celebrities';
-    } else if (category === '19') {
-        catNumber = 27;
-        catName = 'Animals';
-    } else if (category === '20') {
-        catNumber = 28;
-        catName = 'Vehicles';
-    } else if (category === '21') {
-        catNumber = 29;
-        catName = 'Comic Books';
-    } else if (category === '22') {
-        catNumber = 30;
-        catName = 'Gadgets';
-    } else if (category === '23') {
-        catNumber = 31;
-        catName = 'Anime';
-    } else if (category === '24') {
-        catNumber = 32;
-        catName = 'Cartoons';
-    }
+    if (category === '1') {catNumber = 9;catName = 'General';
+    } else if (category === '2') {catNumber = 10; catName = 'Books';
+    } else if (category === '3') {catNumber = 11; catName = 'Film';
+    } else if (category === '4') {catNumber = 12; catName = 'Music';
+    } else if (category === '5') {catNumber = 13; catName = 'Theatre';
+    } else if (category === '6') {catNumber = 14; catName = 'Television';
+    } else if (category === '7') {catNumber = 15; catName = 'Video Games';
+    } else if (category === '8') {catNumber = 16; catName = 'Board Games';
+    } else if (category === '9') {catNumber = 17; catName = 'Science';
+    } else if (category === '10') {catNumber = 18; catName = 'Computers';
+    } else if (category === '11') {catNumber = 19; catName = 'Math';
+    } else if (category === '12') {catNumber = 20; catName = 'Mythology';
+    } else if (category === '13') {catNumber = 21; catName = 'Sports';
+    } else if (category === '14') {catNumber = 22; catName = 'Geography';
+    } else if (category === '15') {catNumber = 23; catName = 'History';
+    } else if (category === '16') {catNumber = 24; catName = 'Politics';
+    } else if (category === '17') {catNumber = 25; catName = 'Art';
+    } else if (category === '18') {catNumber = 26; catName = 'Celebrities';
+    } else if (category === '19') {catNumber = 27; catName = 'Animals';
+    } else if (category === '20') {catNumber = 28; catName = 'Vehicles';
+    } else if (category === '21') {catNumber = 29; catName = 'Comic Books';
+    } else if (category === '22') {catNumber = 30; catName = 'Gadgets';
+    } else if (category === '23') {catNumber = 31; catName = 'Anime';
+    } else {catNumber = 32; catName = 'Cartoons';}
 
     document.getElementById("catPage").style.display = "none";
     document.getElementById("diffPage").style.display = "block";
@@ -258,5 +224,49 @@ function generateRandomFact() {
           });
         }
     })
-    console.log(factArray);
 }
+
+submitBtn.onclick = function() {
+    const name = nameEl.value 
+    let finalscore = ''
+    
+    if (name) {
+        if (catName === 'Random') {
+            finalscore = ' got a ' + finalscoreEl.textContent + ' out of 100 on ' + catName + '.';
+        } else {
+            finalscore = ' got a ' + finalscoreEl.textContent + ' out of 100 on ' + catName + ' on ' + diffSelect + '.';
+        }
+        localStorage.setItem(name, finalscore);
+        highScoreList();
+    }
+}
+
+function highScoreList() {
+    $('#highScoreListEl').children().remove();
+    document.getElementById("catPage").style.display = "none";
+    document.getElementById("diffPage").style.display = "none";
+    document.getElementById("errorPage").style.display = "none";
+    document.getElementById("finalPage").style.display = "none";
+    document.getElementById("quizPage").style.display = "none";
+    document.getElementById("highScorePage").style.display = "block";
+
+    for(let j=0; j < localStorage.length; j++) {
+        const key = localStorage.key(j)
+        const value = localStorage.getItem(key)
+
+        var hsOutput = document.createElement('p');
+        hsOutput.innerHTML += `${key} ${value} <br>`;
+        
+        highScoreListEl.appendChild(hsOutput);
+        console.log(hsOutput)
+    }
+}
+
+$('#clearBtn').on("click", function() {
+    localStorage.clear();
+    highScoreList();
+});
+
+$('#homeBtn').on("click", function() {
+    location.reload();
+});
